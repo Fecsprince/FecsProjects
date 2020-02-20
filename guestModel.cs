4ddcb7e651094dd2e73c94bf4a3556ece5f0c137
Ogu Iheanyichukwu Princewill <61039122+Fecsprince@users.noreply.github.com>
2020-02-19 19:37:23 -0800

    All uploaded files are components of my Portfolio

---
+++ guestModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayParty.Models
+{
+    public class guestModel
+    {
+        [Required(ErrorMessage ="Enter your first Name!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Enter your last Name!")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Enter your phone!")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Enter your email address!")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage ="Enter a valid email address!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Select if you are coming or not!")]
+        public bool? AreYouComing { get; set; }
+
+    }
+}
